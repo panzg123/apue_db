4708ef1f3411de95af4260fd8fa8f09e8e0474f1
panzhengguang <zhengguangpan@hotmail.com>
2015-09-14 11:09:42 +0800

    fixed permission

--- my_src/test_db.c
+++ my_src/test_db.c
@@ -17,13 +17,22 @@ main(void)
 	if ((db = db_open("db4", O_RDWR | O_CREAT | O_TRUNC,
 	  FILE_MODE)) == NULL)
 		err_sys("db_open error");
+	printf("打开成功\n");
 
 	if (db_store(db, "Alpha", "data1", DB_INSERT) != 0)
 		err_quit("db_store error for alpha");
+	else
+		printf("插入成功\n");
+
 	if (db_store(db, "beta", "Data for beta", DB_INSERT) != 0)
 		err_quit("db_store error for beta");
+	else
+			printf("插入成功\n");
+
 	if (db_store(db, "gamma", "record3", DB_INSERT) != 0)
 		err_quit("db_store error for gamma");
+	else
+			printf("插入成功\n");
 
 	db_close(db);
 	exit(0);
